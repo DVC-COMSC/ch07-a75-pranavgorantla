e5c54bbf5b79b83ea57276010e3ff537e2f2b1ac
pranavgorantla <111912611+pranavgorantla@users.noreply.github.com>
2022-11-01 12:36:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ import sys
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
-if len(num1) > len(num2):
-	print ('False')
-	sys.exit(0)
-# ******************************
-# Make your Code
-# ******************************
-
-# print ('True') or ('False')
+# printing original lists
+print("Original list : " + str(num1))
+print("Original sub list : " + str(num2))
+ 
+# using all() to
+# check subset of list
+flag = 0
+if(all(x in num1 for x in num2)):
+    flag = 1
+ 
+# printing result
+if (flag):
+    print("Yes, list is subset of other.")
+else:
+    print("No, list is not subset of other.")
